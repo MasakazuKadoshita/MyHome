c64c3644cfcfef6f4686a432e7c2ee1a4045183f
Masakazu Kadoshita <mkadoshita@gmail.com>
2020-03-01 17:52:29 +0900

    #6 outerHTML, escaping HTML codes

--- src/domtree.js
+++ src/domtree.js
@@ -1,7 +1,21 @@
 window.onload = domtree();
+
 function domtree() {
-  let trees = document.getElementsByTagName('tree');
-  let tree = trees.item(0);
-  let treeAttributes = tree.children;
+  let treeAttributes = document.getElementsByTagName('tree').item(0).children;
   treeAttributes.namedItem('tree_heading').innerHTML = 'Testing Tree structure';
+
+  let escapeHTML = function(str) {
+    return str.replace(/&/g, '&amp;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;')
+        .replace(/"/g, '&quot;')
+        .replace(/'/g, '&#39;');
+  };
+
+  let treeData = treeAttributes.namedItem('tree_data');
+  let root = treeData.children.item(0);
+  let dataShowingWindow = treeAttributes.namedItem('data_showing_window');
+  let codeField =
+      dataShowingWindow.children.item(1).children.namedItem('code_field');
+  codeField.innerHTML = escapeHTML(root.outerHTML);
 }
